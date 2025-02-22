51043501553168553d6a658339f30cb103de0314
Anant3008 <git@stackblitz.com>
2025-02-22 09:22:00 +0530

    Initial commit

---
+++ src/components/usercard.jsx
@@ -0,0 +1,10 @@
+// write the component code here
+function Card() {
+  return (
+    <div>
+      <div>
+        <img src="https://i.pinimg.com/736x/f3/85/d7/f385d78eba93e8b768bcc04bf96fe5a5.jpg" />
+      </div>
+    </div>
+  );
+}
